873fdfd288902a5b17adb23b5e7b41e3ea9044f0
Erol Akarsu <eakarsu@gmail.com>
2022-03-30 16:47:28 -0400

    add local users

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -120,7 +120,12 @@ const config: HardhatUserConfig = {
       initialBaseFeePerGas: 0,
     },
     kovan: {
-      accounts: [`${process.env.KEY}`],
+      //accounts: [`${process.env.KEY}`],
+      accounts: [
+        '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
+        '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d',
+        '0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a',
+      ],
       //hardfork: HARDFORK,
       blockGasLimit: DEFAULT_BLOCK_GAS_LIMIT,
 
