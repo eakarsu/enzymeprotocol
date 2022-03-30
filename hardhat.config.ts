b7a8a107fd807fbedf489de1db8e252ebfe2b3ef
Erol Akarsu <eakarsu@gmail.com>
2022-03-30 16:43:55 -0400

    adding local users

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -131,10 +131,11 @@ const config: HardhatUserConfig = {
       url: 'https://eth-kovan.alchemyapi.io/v2/gURHc2znmQ9VlBz0e4qlQSsXia-SgqOj',
     },
     localhost: {
+      //Remove accounts before testing unlock mainnet users and test contracts
       accounts: [
-        //'0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
-        //'0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d',
-        //'0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a',
+        '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
+        '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d',
+        '0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a',
       ],
       chainId: 1,
       //chainId: 31337,
